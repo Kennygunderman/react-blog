f16dcb0bb7f2914dbb9a251d4888b60237648135
Kenny <kennygunderman@gmail.com>
2021-02-05 16:45:24 -0600

    Fix overlay issue

--- src/components/FeaturedBlogItem/FeaturedBlogItem.js
+++ src/components/FeaturedBlogItem/FeaturedBlogItem.js
@@ -4,21 +4,23 @@ import classes from './FeaturedBlogItem.css';
 import { createDescription } from '../../util/StringUtils'
 
 const featuredBlogItem = (props) => (
-    <Card onClick={props.clickHandler} className={classes.FeaturedBlogCardItem}>
+    <Card onClick={() => props.clickHandler(props.item)} className={classes.FeaturedBlogCardItem}>
         <CardMedia image={props.item.image} title={'Thumbnail'}>
-            <CardActionArea className={classes.Overlay}>
-                <CardContent className={classes.CardContent}>
-                    <Typography component="h2" variant="h3" className={classes.Header}>
-                        {props.item.title}
+            <div className={classes.Overlay}>
+                <CardActionArea>
+                    <CardContent className={classes.CardContent}>
+                        <Typography component="h2" variant="h3" className={classes.Header}>
+                            {props.item.title}
+                        </Typography>
+                        <Typography variant="h5" className={classes.Desc}>
+                            {createDescription(props.item.summary, 120)}
+                        </Typography>
+                        <Typography variant="subtitle1" className={classes.ContinueReadingText}>
+                            Continue reading...
                     </Typography>
-                    <Typography variant="h5" className={classes.Desc}>
-                        {createDescription(props.item.summary, 120)}
-                    </Typography>
-                    <Typography variant="subtitle1" className={classes.ContinueReadingText}>
-                        Continue reading...
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
+                    </CardContent>
+                </CardActionArea>
+            </div>
         </CardMedia>
     </Card>
 )
