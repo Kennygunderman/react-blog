a919c4ec0e39a03ad6fad7c6c2058eef4be07bd6
Kenny <kennygunderman@gmail.com>
2021-02-05 16:18:25 -0600

    Update clickhandler to pass blog item to detail

--- src/containers/BlogDetail/BlogDetail.js
+++ src/containers/BlogDetail/BlogDetail.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 class BlogDetail extends Component {
+    state = {
+        item: null
+    }
+
+    componentDidMount() {
+        this.setState({ item: this.props.history.location.state.item })
+    }
+
     render() {
         return (
             <div>
