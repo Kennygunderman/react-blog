8578d91416b211ecd7f1100b2b3378dc217a5e94
kennygunderman <kennygunderman@gmail.com>
2021-02-08 11:10:11 -0500

    Add home page logic

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,8 +1,9 @@
-import { AppBar, Toolbar, Typography, Box, Button } from '@material-ui/core';
+import { AppBar, Toolbar as MaterialToolbar, Typography, Box, Button, Link } from '@material-ui/core';
 import { auth, signOut } from '../../auth/auth'
 import { connect } from 'react-redux';
 import { AUTH } from '../../store/actions';
 import firebase from '../../firebase';
+import { withRouter } from 'react-router-dom';
 
 const handleAuth = (onAuthUser) => {
     const success = (token, user) => {
@@ -29,33 +30,40 @@ const handleSignOut = (onAuthUser) => {
     signOut(success, failure);
 }
 
+const onHomeClicked = (history) => {
+    history.push('/');
+}
+
 const toolbar = (props) => {
-    firebase.auth().onAuthStateChanged(function (user) {
+    firebase.auth().onAuthStateChanged((user) => {
         if (user) {
             // User is signed in.
             props.onAuthUser({ isAuthed: true, user: user });
         }
     });
 
-    return (<AppBar position='static'>
-        <Toolbar variant='regular' color='primary'>
-            <Box display='flex' flexGrow={1}>
-                <Typography variant='h6'>Kenny Gunderman</Typography>
-            </Box>
+    return (
+        <AppBar position='static'>
+            <MaterialToolbar variant='regular' color='primary'>
+                <Box display='flex' flexGrow={1}>
+                    <Typography variant='h6'><Link href="#" onClick={() => onHomeClicked(props.history)} color="secondary">Kenny Gunderman</Link></Typography>
+                </Box>
 
-            <Button
-                variant="outlined"
-                color="secondary"
-                onClick={props.isAuthed ? () => handleSignOut(props.onAuthUser) : () => handleAuth(props.onAuthUser)}>
-                {props.isAuthed ? 'Sign out' : 'Sign in'}
-            </Button>
+                <Button
+                    variant="outlined"
+                    color="secondary"
+                    onClick={props.isAuthed ? () => handleSignOut(props.onAuthUser) : () => handleAuth(props.onAuthUser)}>
+                    {props.isAuthed ? 'Sign out' : 'Sign in'}
+                </Button>
 
-        </Toolbar>
-    </AppBar>)
+            </MaterialToolbar>
+        </AppBar>
+    )
 }
 
 const mapStateToProps = state => {
     return {
+        ...state,
         isAuthed: state.isAuthed,
         user: state.user
     };
@@ -67,4 +75,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(toolbar);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(toolbar));
